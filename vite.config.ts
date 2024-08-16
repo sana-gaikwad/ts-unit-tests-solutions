a8aed9547c25f5bd1d78b947499b8d4cb1752c6e
Eric Liu <eric@nomoss.co>
2024-08-16 11:10:07 +1000

    fix: specify base URL for GitHub pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import type { UserConfig } from 'vite'
 import { coverageConfigDefaults } from 'vitest/config'
 
 export default {
+  base: '/ts-unit-tests/',
   test: {
     coverage: {
         exclude: ['**/main.ts', ...coverageConfigDefaults.exclude]
